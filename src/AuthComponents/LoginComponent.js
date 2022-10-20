8e73d4d14ff99dfe1c45f150dcd0ddb66d8ae69d
Konrad <foksie999@gmail.com>
2022-10-21 00:43:36 +0200

    21.10

--- src/AuthComponents/LoginComponent.js
+++ src/AuthComponents/LoginComponent.js
@@ -9,16 +9,15 @@ import {
 }
 from 'mdb-react-ui-kit';
 import Button from '@mui/material/Button';
-
 import Logo from "../Images/Logo.png"
 import AuthServices from "../Services/AuthServices"
-
 
 function LoginComponent() {
 
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
+  /*
    const loginUser = () => {
       AuthServices.loginUser(login,password)
       .then(response => response.json())   
@@ -31,6 +30,7 @@ function LoginComponent() {
           console.log(error);
         });
   }
+*/
 
   return (
     <MDBContainer fluid>
@@ -42,17 +42,12 @@ function LoginComponent() {
             </div>
           <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
             <MDBCardBody className='p-5 w-100 d-flex flex-column'>
-
               <h3 className="text-center">Zaloguj</h3>
               <p className="text-black-50 mb-3">Please enter your login and password!</p>
-
               <MDBInput wrapperClass='mb-4 w-100' label='Nazwa użytkownika' type='login' size="lg" onChange={(e) => setLogin(e.target.value)}/>
               <MDBInput wrapperClass='mb-4 w-100' label='Hasło' type='password' size="lg" onChange={(e) => setPassword(e.target.value)}/>
-
               <p className="text-center">Nie masz konta? <a href="#!">Zarejestruj</a></p>
-
               <Button variant="contained">Login</Button>
-
             </MDBCardBody>
           </MDBCard>
 
--- src/AuthComponents/RegisterComponent.js
+++ src/AuthComponents/RegisterComponent.js
@@ -22,14 +22,15 @@ function RegisterComponent() {
   const [address, setAddress] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  /*
   const registerUser = (e) => {
     const newUser = {name,surname,login,email,password,address}
     AuthServices.registerUser(newUser)
 }
 
+*/
   return (
     <MDBContainer fluid>
-
       <MDBRow className='d-flex justify-content-center align-items-center h-100'>
         <MDBCol col='13'>
         <div className="text-center">
@@ -38,28 +39,22 @@ function RegisterComponent() {
             </div>
           <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
             <MDBCardBody className='p-5 w-100 d-flex flex-column'>
-
               <h3 className="text-center">Rejestracja</h3>
               <p className="text-black-50 mb-3 text-center">Niepoprawne haslo</p>
-
               <MDBRow>
                 <MDBCol col='6'>
                   <MDBInput wrapperClass='mb-4' label='Imie' id='name' type='text' onChange={(e) => setName(e.target.value)}/>
                 </MDBCol>
-
                 <MDBCol col='6'>
                   <MDBInput wrapperClass='mb-4' label='Nazwisko' id='surname' type='text' onChange={(e) => setSurname(e.target.value)}/>
                 </MDBCol>
-              </MDBRow>
-             
+              </MDBRow>            
               <MDBInput wrapperClass='mb-4 w-100' label='Nazwa Użytkownika' id='userName' type='text' size="lg" onChange={(e) => setLogin(e.target.value)}/>
               <MDBInput wrapperClass='mb-4 w-100' label='Adres Email' id='Email' type='text' size="lg" onChange={(e) => setEmail(e.target.value)}/>
               <MDBInput wrapperClass='mb-4 w-100' label='Hasło' id='Password' type="password" size="lg" onChange={(e) => setPassword(e.target.value)}/>
               <MDBInput wrapperClass='mb-4 w-100' label='Address' id='addresss' type='text' size="lg" onChange={(e) => setAddress(e.target.value)}/>
               <MDBInput wrapperClass='mb-4 w-100' label='Powtórz hasło' id='repeat_password' type="password" size="lg" onChange={(e) => setConfirmPassword(e.target.value)}/>
-
               <Button variant="contained">Zarejestruj</Button>
-
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
