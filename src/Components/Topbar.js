52e736b83abfced8376db908dabbbed4c43a0e64
Konrad <foksie999@gmail.com>
2022-10-24 20:54:49 +0200

    24.10

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -1,4 +1,11 @@
 import React, { useState,useEffect } from 'react';
+import Badge from '@mui/material/Badge';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import LogoutIcon from '@mui/icons-material/Logout';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 import User from "../Images/user.png";
 import Logo from "../Images/Logo.png";
 import ShoppingCart from "../Images/shopping_cart.png";
@@ -6,12 +13,15 @@ import TopBarServices from "../Services/TopBarServices";
 import SearchBar from '../SharedComponent/SearchBar';
 import StyledLink from '../SharedComponent/StyledLink';
 import AuthServices from '../Services/AuthServices';
+import StyledDivider from '../SharedComponent/StyledDivider';
 import './Topbar.css';
+
 
 function Topbar({user,setUser}) {
 
     const [allProducts, setAllProducts] = useState([]);
     const [searchWord, setSearchWord] = useState('');
+    const [anchorEl, setAnchorEl] = useState(null);
 
     useEffect(() => {
         TopBarServices.getProductsFiltered(searchWord).then((response) => {
@@ -26,6 +36,16 @@ function Topbar({user,setUser}) {
             setUser(null);
             window.location.reload(false)
        }
+
+       const handleClickMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+    
+      const handleCloseMenu = () => {
+        setAnchorEl(null);
+      };
+  
+      const open = Boolean(anchorEl);
 
     return (
         <div className="topbar__wrapper" role="navigation">       
@@ -45,13 +65,41 @@ function Topbar({user,setUser}) {
                                 <span className = "topbar__rightBox_spanItem">Zaloguj</span>
                              </StyledLink>
                                 ) : (
-                                <span className = "topbar__rightBox_spanItem" onClick={()=>logoutUser()}>Wyloguj</span>     
+                                <>
+                                <span className = "topbar__rightBox_spanItem" onClick={handleClickMenu}>Twoje Konto</span>
+                                <Popover
+                                open={open}
+                                anchorEl={anchorEl}
+                                onClose={handleCloseMenu}
+                                anchorOrigin={{
+                                  vertical: 'bottom',
+                                  horizontal: 'left',
+                                }}
+                                >
+                                    <StyledLink to={"/"}>
+                                        <Typography style = {{cursor:'pointer'}} sx = {{p: 1}}><ManageAccountsIcon/> Profil</Typography>
+                                    </StyledLink>
+                                    <StyledDivider/>
+                                    <StyledLink to={"/RecentOrders"}>
+                                        <Typography style = {{cursor:'pointer'}} sx = {{p: 1}}><TextSnippetIcon/> Twoje zamówienia</Typography>
+                                    </StyledLink>
+                                    <StyledDivider/>
+                                    {
+                                    user.role === 'Admin' && <Typography style = {{cursor:'pointer'}} sx = {{p: 1}}><AdminPanelSettingsIcon/> Panel administratora</Typography>
+                                    }
+                                    <StyledDivider/>
+                                    <Typography style = {{cursor:'pointer'}} sx = {{p: 1}} onClick = {() => logoutUser()}><LogoutIcon/> Wyloguj</Typography>
+                                </Popover> 
+                                </>
+
                                 )
                             }           
                 </div>
                 <StyledLink to={"/Cart"}>
-                    <div className = "topbar__rightBox__cart">                            
-                        <img src={ShoppingCart} alt = "ShoppingCart"/>          
+                    <div className = "topbar__rightBox__cart"> 
+                    <Badge badgeContent={4} color="success">                           
+                        <img src={ShoppingCart} alt = "ShoppingCart"/>    
+                    </Badge>      
                         <span className = "topbar__rightBox_spanItem">Twój Koszyk</span>
                     </div>
                 </StyledLink>  
