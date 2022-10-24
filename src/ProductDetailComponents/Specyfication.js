ae74447a8c7df19485857d86191b905543579715
Konrad <foksie999@gmail.com>
2022-10-24 22:19:18 +0200

    24.10

--- src/ProductDetailComponents/Specyfication.js
+++ src/ProductDetailComponents/Specyfication.js
@@ -10,10 +10,10 @@ function Specyfication({details}) {
       {details &&
         Object.keys(details).slice(1).map((key, i)=>(
             <> 
-            <tr key={i}>
-              <td> {key.charAt(0).toUpperCase() + key.slice(1)}  </td>
-              <td>{details[key]} </td>
-            </tr>   
+              <tr key={i}>
+                <td> {key.charAt(0).toUpperCase() + key.slice(1)}  </td>
+                <td>{details[key]} </td>
+              </tr>   
             </>  
         ))
     }
